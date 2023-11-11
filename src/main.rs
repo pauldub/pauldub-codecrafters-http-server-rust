b2abd62c7e923f17710973278e15a17f8a238f8a
Paul d'Hubert <paul.dhubert@pm.me>
2023-11-11 09:15:13 +0100

    feat: handle multiple concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::net::TcpListener;
-use std::io::{Read, Write};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub mod http {
-    use nom::{IResult, bytes::complete::{tag, take_till1}, character::complete::{space1, line_ending}};
+    use nom::{
+        bytes::complete::{tag, take_till1},
+        character::complete::{line_ending, space1},
+        IResult,
+    };
 
     #[derive(Debug)]
     pub struct Request {
@@ -10,7 +14,7 @@ pub mod http {
         pub path: String,
         pub http_version: String,
     }
-    
+
     impl Request {
         pub fn from_bytes(bytes: &[u8]) -> IResult<&[u8], Self> {
             let (input, method) = tag("GET")(bytes)?;
@@ -20,53 +24,52 @@ pub mod http {
             let (input, http_version) = tag("HTTP/1.1")(input)?;
             let (input, _) = line_ending(input)?;
 
-            let request = Request{
+            let request = Request {
                 method: String::from_utf8_lossy(method).to_string(),
                 path: String::from_utf8_lossy(path).to_string(),
                 http_version: String::from_utf8_lossy(http_version).to_string(),
             };
-            
+
             Ok((input, request))
         }
     }
-    
+
     #[derive(Debug)]
     pub struct Header {
         pub name: String,
         pub value: String,
     }
-    
-    impl Header { 
+
+    impl Header {
         pub fn from_bytes(bytes: &[u8]) -> IResult<&[u8], Self> {
             let (input, name) = take_till1(|c| c == b':')(bytes)?;
             let (input, _) = tag(": ")(input)?;
             let (input, value) = take_till1(|c| c == b'\r')(input)?;
             let (input, _) = line_ending(input)?;
-    
-            let header = Header{
+
+            let header = Header {
                 name: String::from_utf8_lossy(name).to_string(),
                 value: String::from_utf8_lossy(value).to_string(),
             };
-            
+
             Ok((input, header))
         }
-        
+
         pub fn parse_all(bytes: &[u8]) -> IResult<&[u8], Vec<Self>> {
             let mut headers = Vec::new();
             let mut input = bytes;
-    
+
             loop {
                 let (leftover, header) = Header::from_bytes(input)?;
                 input = leftover;
                 headers.push(header);
-    
-                println!("input.len(): {}", input.len());
+
                 // Stop when there are two lines separating the headers from the body
                 if input.len() >= 4 && &input[0..4] == b"\r\n\r\n" {
                     input = &input[4..];
                     break;
                 }
-                
+
                 if input.len() == 0 {
                     break;
                 }
@@ -75,61 +78,72 @@ pub mod http {
                     break;
                 }
             }
-    
+
             Ok((input, headers))
         }
     }
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:4221").await.unwrap();
 
     println!("Listening for connections on 127.0.0.1:4221");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
+    loop {
+        let (mut socket, _) = listener.accept().await.unwrap();
 
-                let mut buffer = [0 as u8; 1024];
-                let read_size = stream.read(&mut buffer).unwrap();
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
 
-                println!("read {} bytes", read_size);
-                
-                let raw_request = &buffer[0..read_size];
-                let (input, request) = http::Request::from_bytes(raw_request).unwrap();
-                println!("request: {:?}", request);
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => {
+                        // connection was closed
+                        break;
+                    }
+                    Ok(n) => {
+                        println!("read {} bytes", n);
 
-                let (_input, headers) = http::Header::parse_all(input).unwrap();
-                println!("request headers: {:?}", headers);
-                
-                if request.path == "/" {
-                    write!(&mut stream, "HTTP/1.1 200 OK\r\n\r\n").unwrap();
-                } else if request.path.starts_with("/echo/") {
-                    match request.path.split_once("/echo/") {
-                        Some((_, message)) => {
-                            write!(&mut stream, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", message.len(), message).unwrap();
-                        },
-                        None => {
-                            write!(&mut stream, "HTTP/1.1 400 Bad Request\r\n\r\n").unwrap();
+                        let raw_request = &buf[0..n];
+                        let (input, request) = http::Request::from_bytes(raw_request).unwrap();
+                        println!("request: {:?}", request);
+
+                        let (_input, headers) = http::Header::parse_all(input).unwrap();
+                        println!("request headers: {:?}", headers);
+
+                        if request.path == "/" {
+                            socket.write(b"HTTP/1.1 200 OK\r\n\r\n").await.unwrap();
+                        } else if request.path.starts_with("/echo/") {
+                            match request.path.split_once("/echo/") {
+                                Some((_, message)) => {
+                                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", message.len(), message);
+                                    socket.write_all(response.as_bytes()).await.unwrap();
+                                }
+                                None => {
+                                    socket.write(b"HTTP/1.1 400 Bad Request\r\n\r\n").await.unwrap();
+                                }
+                            }
+                        } else if request.path == "/user-agent" {
+                            match headers.iter().find(|h| h.name == "User-Agent") {
+                                Some(header) => {
+                                    let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", header.value.len(), header.value);
+                                    socket.write_all(response.as_bytes()).await.unwrap();
+                                }
+                                None => {
+                                    socket.write(b"HTTP/1.1 400 Bad Request\r\n\r\n").await.unwrap();
+                                }
+                            }
+                        } else {
+                            socket.write(b"HTTP/1.1 404 Not Found\r\n\r\n").await.unwrap();
                         }
                     }
-                } else if request.path == "/user-agent" {
-                    match headers.iter().find(|h| h.name == "User-Agent") {
-                        Some(header) => {
-                            write!(&mut stream, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", header.value.len(), header.value).unwrap();
-                        },
-                        None => {
-                            write!(&mut stream, "HTTP/1.1 400 Bad Request\r\n\r\n").unwrap();
-                        }
+                    Err(_) => {
+                        // an error occurred, we can handle it here or just break
+                        break;
                     }
-                } else {
-                    write!(&mut stream, "HTTP/1.1 404 Not Found\r\n\r\n").unwrap();
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
