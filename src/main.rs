aa2c56c7d3962d9b0878e55e499ee04c3b7d48ba
Paul d'Hubert <paul.dhubert@pm.me>
2023-11-11 08:16:34 +0100

    feat: return a 200 OK response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,8 +8,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                write!(&mut stream, "HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
