4a10a0493e00515e17d6f31d3abb2915a1c26d1d
Paul d'Hubert <paul.dhubert@pm.me>
2023-11-11 08:18:33 +0100

    feat: read request before sending response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpListener;
-use std::io::Write;
+use std::io::{Read, Write};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -10,6 +10,10 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
+
+                let mut request: Vec<u8> = vec![];
+
+                stream.read_to_end(&mut request).unwrap();
 
                 write!(&mut stream, "HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
